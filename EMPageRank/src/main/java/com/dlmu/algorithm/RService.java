007f89a2db3346add30b2fe377ca97d5eb45b05e
yuysunny <yuyysunny@gmail.com>
2015-09-05 17:24:59 -0400

    Update RService.java

--- EMPageRank/src/main/java/com/dlmu/algorithm/RService.java
+++ EMPageRank/src/main/java/com/dlmu/algorithm/RService.java
@@ -47,9 +47,9 @@ public class RService {
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd-HHmmss");  
         Date date = new Date();  
         String str = simpleDateFormat.format(date);  
-        c.eval("png(file='/Users/yyy/Desktop/acm40_300.png',width=400,height=350,res=72)");
+        // c.eval("png(file='/Users/yyy/Desktop/acm40_300.png',width=400,height=350,res=72)");
 
-//        c.eval("png(file='"+JsonUtil.plot_file+str+".png',width=400,height=350,res=72)");
+        c.eval("png(file='"+JsonUtil.plot_file+str+".png',width=400,height=350,res=72)");
 		c.parseAndEval("print(ggplot(data=mdf,aes(x=iteration,y=value,group=variable,colour=variable))+geom_line());dev.off()");
 		
 
@@ -60,45 +60,10 @@ public class RService {
 //        c.eval("ggsave(file='/Users/yyy/Desktop/yyy.pdf')");
         c.close();
 	}
-	public int setModelFile(String modelFile,int year) throws Exception {
-		this.rf_path = modelFile;
-		c.eval("author<-read.table('"+modelFile+"',header=T)");
-//		c.eval("detach(author)");
-		c.eval("attach(author)");
-		c.eval("model1=lm(num~year,data=author)");
-		c.eval("coeffs=coefficients(model1)");
-		REXP x = c.eval("value=coeffs[1]+"+year+"*coeffs[2]");
-//		System.out.println(x.asInteger());
-		return x.asInteger();
-		
-	}
+
 	
-	public double predictImportance(String modelFile,int year) throws Exception {
-		this.rf_path = modelFile;
-		c.eval("author<-read.table('"+modelFile+"',header=T)");
-//		c.eval("detach(author)");
-		c.eval("attach(author)");
-		c.eval("model1=lm(score~year,data=author)");
-		c.eval("coeffs=coefficients(model1)");
-		REXP x = c.eval("value=coeffs[1]+"+year+"*coeffs[2]");
-//		System.out.println(x.asInteger());
-		return x.asDouble();
-		
-	}
-	public double calcSingleDistance(String feat) throws Exception {
-		c.eval("test = c(" + feat + ")");
-		REXP x = c.eval("predict(rf, test, proximity=FALSE, type='prob')[,1]");
-		return x.asDouble();
-	}
 	
-	public double[] calcDistances(String dist_file) throws Exception {
-		
-		c.eval("test = read.table('" + dist_file + "', header=TRUE, sep=',')");
-		REXP x = c.eval("predict(rf, newdata=test, proximity=FALSE, type='prob')[,1]");
-		
-		double[] probs = x.asDoubles();
-		return probs;
-	}
+	
 
 	public void startup() throws Exception {
 		if (c != null)
@@ -110,73 +75,11 @@ public class RService {
 		c = null;
 	}
 
-	public void predict(int year) throws Exception
-	{
-		String path="/Users/yyy/Documents/workspace/ABMGraph/author_split_inEachYear_284/";
-    	File file=new File(path);
-    	String filelist[]=file.list();
-    	BufferedWriter outPut= new BufferedWriter(new FileWriter("./author_284_publication_num"));
-		
-    	for(int i=0;i<filelist.length;i++)
-    	{
-    		RService r=new RService();
-//    		System.out.println(path+filelist[i]);
-        	int num=r.setModelFile(path+filelist[i],year);
-        	outPut.write((i+1)+","+filelist[i].split("_")[1]+","+num);
-        	outPut.newLine();
-        	outPut.flush();
-    	}
-    	outPut.close();
-	}
+
 	// java -Dcsx.boot=bootstrap/disambiguation.txt -Dcsx.conf=conf edu.psu.citeseerx.disambiguation.DisambiguationService
     public static void main(String[] args) throws Exception {
-    	String path="/Users/yyy/Documents/workspace/ABMGraph/paper_number_compare";
-    	BufferedWriter outPut= new BufferedWriter(new FileWriter("./coauthor_number_predict"));
-    	RService r=new RService();
-    	for(int i=2005;i<2010;i++){
-    		int score=r.setModelFile(path,i);
-    		outPut.write(""+score);
-    		outPut.newLine();
-    		outPut.flush();
-    	}
-    	outPut.close();
+    	
     } //- main
-    public static void predictImportance()throws Exception 
-    {
-    	String path="/Users/yyy/Documents/workspace/ABMGraph/author_split_importance_inEachYear/";
-    	File file=new File(path);
-    	String filelist[]=file.list();
-    	BufferedWriter outPut= new BufferedWriter(new FileWriter("./author_284_importance_2009"));
-    	HashMap<Integer,Double> count1= new HashMap<Integer,Double> ();
-    	for(int i=0;i<filelist.length;i++)
-    	{
-    		RService r=new RService();
-    		System.out.println(path+filelist[i]);
-        	double score=r.predictImportance(path+filelist[i],2009);
-        	int key1=Integer.valueOf(filelist[i].split("_")[1]);
-			Double value=score;
-			count1.put(key1, value);
-//        	outPut.write((i+1)+","+filelist[i].split("_")[1]+","+num);
-//        	outPut.newLine();
-//        	outPut.flush();
-    	}
+    
 
-		List<Map.Entry<Integer, Double>> infoIds = new ArrayList<Map.Entry<Integer, Double>>(count1.entrySet());  
-		  
-        Collections.sort(infoIds, new Comparator<Map.Entry<Integer, Double>>() {  
-            public int compare(Map.Entry<Integer, Double> o1,  
-                    Map.Entry<Integer, Double> o2) {  
-//              System.out.println(o1.getKey()+"   ===  "+o2.getKey());  
-                return (Double.valueOf(o2.getValue().toString()).compareTo(Double.valueOf(o1.getValue().toString())));  
-            }  
-        }); 
-        for (int i = 0; i < infoIds.size(); i++) {  
-            String id = infoIds.get(i).toString();  
-            String str[]=id.split("=");
-            outPut.append((i+1)+","+str[0]+","+str[1]);
-            outPut.newLine();
-            outPut.flush();
-        }  
-		outPut.close();
-    }
 }
