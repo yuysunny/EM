95b863786af25fe281aa528d4cf580fa3f89e433
yuysunny <yuyysunny@gmail.com>
2015-09-05 17:27:04 -0400

    Update RService.java

--- EMPageRank/src/main/java/com/dlmu/algorithm/RService.java
+++ EMPageRank/src/main/java/com/dlmu/algorithm/RService.java
@@ -1,31 +1,10 @@
 package com.dlmu.algorithm;
 
-import java.awt.Image;
-import java.awt.Toolkit;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.rosuda.REngine.Rserve.RConnection;
-import org.rosuda.REngine.REXP;
 
-
-/**
- * DisambiguationService
- * 
- * @author Puck Treeratpituk
- * @version $Rev$ $Date$
- */
 public class RService {
 	RConnection c;
 	String rf_path= "";
@@ -47,9 +26,9 @@ public class RService {
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd-HHmmss");  
         Date date = new Date();  
         String str = simpleDateFormat.format(date);  
-        // c.eval("png(file='/Users/yyy/Desktop/acm40_300.png',width=400,height=350,res=72)");
+        c.eval("png(file='/Users/yyy/Desktop/acm40_300.png',width=400,height=350,res=72)");
 
-        c.eval("png(file='"+JsonUtil.plot_file+str+".png',width=400,height=350,res=72)");
+//        c.eval("png(file='"+JsonUtil.plot_file+str+".png',width=400,height=350,res=72)");
 		c.parseAndEval("print(ggplot(data=mdf,aes(x=iteration,y=value,group=variable,colour=variable))+geom_line());dev.off()");
 		
 
@@ -60,26 +39,10 @@ public class RService {
 //        c.eval("ggsave(file='/Users/yyy/Desktop/yyy.pdf')");
         c.close();
 	}
-
-	
-	
 	
 
-	public void startup() throws Exception {
-		if (c != null)
-			c.shutdown();
-		c = new RConnection();
-	}
-	public void shutdown() throws Exception {
-		c.shutdown();
-		c = null;
-	}
-
-
+	
 	// java -Dcsx.boot=bootstrap/disambiguation.txt -Dcsx.conf=conf edu.psu.citeseerx.disambiguation.DisambiguationService
     public static void main(String[] args) throws Exception {
-    	
     } //- main
-    
-
 }
